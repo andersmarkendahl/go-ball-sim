8e3dc8456421622f9d52f3e5ad23c860498ffddd
Anders Markendahl <andersmarkendahl@gmail.com>
2020-01-06 14:39:16 +0100

    src: printStatus function

--- main.go
+++ main.go
@@ -10,17 +10,21 @@ type ball struct {
 	radius     float64
 }
 
+func printStatus(balls ...ball) {
+	fstring := "Ball %d: pos(%2.2f, %2.2f) vel(%2.2f, %2.2f)\n"
+	for i, b := range balls {
+		fmt.Printf(fstring, i, b.posX, b.posY, b.velX, b.velY)
+	}
+}
+
 func main() {
 
 	var nballs int
 
 	fmt.Println("Welcome to ball simulator! Please insert an integer")
-
 	fmt.Scanf("%d", &nballs)
 	fmt.Printf("You have entered %d balls \n", nballs)
-
 	balls := make([]ball, nballs)
-	fmt.Println("Empty Balls:", balls)
 
 	for i := range balls {
 		balls[i].posX = 0
@@ -29,6 +33,6 @@ func main() {
 		balls[i].velY = 0
 		balls[i].radius = 1
 	}
-	fmt.Println("Set Balls:", balls)
+	printStatus(balls...)
 
 }
