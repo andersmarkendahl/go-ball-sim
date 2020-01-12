49faa6b3ce2cc9516882cfe8e542134c7130540a
Anders Markendahl <andersmarkendahl@gmail.com>
2020-01-12 09:15:19 +0100

    test: Added formatting for failure and initial Velocity tests

--- pkg/objects/objects_test.go
+++ pkg/objects/objects_test.go
@@ -34,14 +34,35 @@ func TestPosition(t *testing.T) {
 		{Object{X: 100, Y: 200, VX: 20, VY: 40}, Object{X: 108, Y: 216, VX: 20, VY: 40}, 2.5},
 		{Object{X: 100, Y: 200, VX: 20, VY: 40}, Object{X: 112.5, Y: 225, VX: 20, VY: 40}, 1.6},
 	} {
+		orig := c.in
 		c.in.Position(c.dt)
 		if isDifferent(c.in, c.want) {
-			t.Error("Position() failed", c.in, c.want)
+			t.Errorf("Position(%f) \nin: %+v\nwanted: %+v\nresult: %+v", c.dt, orig, c.want, c.in)
 		}
 	}
 }
 
 func TestVelocity(t *testing.T) {
+	for _, c := range []struct {
+		in, want   Object
+		ax, ay, dt float64
+	}{
+		// Velocity update in all directions
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 0, VY: 0}, 0, 0, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 1, VY: 0}, 1, 0, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 0, VY: 1}, 0, 1, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 1, VY: 1}, 1, 1, 1},
+		// Float values
+//		{Object{X: 1.1, Y: 2.2, VX: 1.1, VY: 2.2}, Object{X: 2.2, Y: 4.4, VX: 1.1, VY: 2.2}, 1},
+		// dt variation
+//		{Object{X: 100, Y: 200, VX: 20, VY: 40}, Object{X: 102, Y: 204, VX: 20, VY: 40}, 10},
+	} {
+		orig := c.in
+		c.in.Velocity(c.ax, c.ay, c.dt)
+		if isDifferent(c.in, c.want) {
+			t.Errorf("Velocity(%f, %f, %f) \nin: %+v\nwanted: %+v\nresult: %+v", c.ax, c.ay, c.dt, orig, c.want, c.in)
+		}
+	}
 
 }
 
