2c4e342fa2422b0c4550a8fd1c4d14bedccd8ff4
Anders Markendahl <andersmarkendahl@gmail.com>
2020-01-12 12:43:43 +0100

    test: object additional Velocity tests

--- pkg/objects/objects_test.go
+++ pkg/objects/objects_test.go
@@ -54,10 +54,18 @@ func TestVelocity(t *testing.T) {
 		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 1, VY: 0}, 1, 0, 1},
 		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 0, VY: 1}, 0, 1, 1},
 		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 1, VY: 1}, 1, 1, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: -1, VY: 0}, -1, 0, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 0, VY: -1}, 0, -1, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: -1, VY: -1}, -1, -1, 1},
 		// Float values
-		//		{Object{X: 1.1, Y: 2.2, VX: 1.1, VY: 2.2}, Object{X: 2.2, Y: 4.4, VX: 1.1, VY: 2.2}, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 1.5, VY: 2.5}, 1.5, 2.5, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: -1.5, VY: 2.5}, -1.5, 2.5, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: 1.5, VY: -2.5}, 1.5, -2.5, 1},
+		{Object{X: 0, Y: 0, VX: 0, VY: 0}, Object{X: 0, Y: 0, VX: -1.5, VY: -2.5}, -1.5, -2.5, 1},
 		// dt variation
-		//		{Object{X: 100, Y: 200, VX: 20, VY: 40}, Object{X: 102, Y: 204, VX: 20, VY: 40}, 10},
+		{Object{X: 100, Y: 200, VX: 20, VY: 40}, Object{X: 100, Y: 200, VX: 21, VY: 41}, 10, 10, 10},
+		{Object{X: 100, Y: 200, VX: 20, VY: 40}, Object{X: 100, Y: 200, VX: 24, VY: 44}, 10, 10, 2.5},
+		{Object{X: 100, Y: 200, VX: 20, VY: 40}, Object{X: 100, Y: 200, VX: 26.25, VY: 46.25}, 10, 10, 1.6},
 	} {
 		orig := c.in
 		c.in.Velocity(c.ax, c.ay, c.dt)
