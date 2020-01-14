af90ed14ce476d4d5d195b9555057c964b38a97f
Anders Markendahl <andersmarkendahl@gmail.com>
2020-01-14 08:48:27 +0100

    test: gfxutil package

--- pkg/gfxutil/gfxutil_test.go
+++ pkg/gfxutil/gfxutil_test.go
@@ -7,4 +7,33 @@ import (
 // LoadPng opens a png image and stores a decoded image
 func TestLoadPng(t *testing.T) {
 
+	// Positive tests
+	for _, c := range []string{
+		"testdata/example.png",
+	} {
+		img, err := LoadPng(c)
+		if err != nil {
+			t.Errorf("LoadPng(%s): %s", c, err)
+		}
+		if img == nil {
+			t.Errorf("LoadPng(%s): empty image", c)
+		}
+	}
+	// Negative tests
+	for _, c := range []string{
+		"testdata/donotexist.png",
+		"testdata/example.jpg",
+		"testdata/garbage",
+	} {
+		_, err := LoadPng(c)
+		if err == nil {
+			t.Errorf("LoadPng(%s): loadable", c)
+		}
+	}
+}
+
+func TestPrintImage(t *testing.T) {
+
+	// Nothing to test, ebiten does not provide error codes here
+
 }
