a6a47ab7e242e35e6cc4f9429ffddbd646848aae
Anders Markendahl <andersmarkendahl@gmail.com>
2020-01-18 13:23:01 +0100

    mathutil: Added Magnitude and DotProduct functions

--- pkg/mathutil/mathutil.go
+++ pkg/mathutil/mathutil.go
@@ -2,11 +2,12 @@ package mathutil
 
 import (
 	"errors"
+	"math"
 	"math/rand"
 	"time"
 )
 
-// RandInRange returns a random number in given range
+// RandInRange returns a random number in given float range
 func RandInRange(min, max float64) (float64, error) {
 
 	if min >= max {
@@ -16,4 +17,14 @@ func RandInRange(min, max float64) (float64, error) {
 	rand.Seed(time.Now().UnixNano())
 
 	return rand.Float64()*(max-min) + min, nil
+}
+
+// Magnitude of a vector
+func Magnitude(x, y float64) float64 {
+	return math.Sqrt(x*x + y*y)
+}
+
+// DotProduct (inner product) of two vectors (x1, y1) (x2, y2)
+func DotProduct(x1, y1, x2, y2 float64) float64 {
+	return x1*x2 + y1*y2
 }
