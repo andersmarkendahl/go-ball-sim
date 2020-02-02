a6772004713681bf59f43c1bda7502a201282e9e
Anders Markendahl <andersmarkendahl@gmail.com>
2020-02-02 15:33:51 +0100

    test: Finalized ball tests (fixed #17)

--- internal/pkg/ball/ball_test.go
+++ internal/pkg/ball/ball_test.go
@@ -3,12 +3,21 @@ package ball
 import (
 	"github.com/Aoana/ball-sim-go/assets/images"
 	"github.com/Aoana/ball-sim-go/pkg/gfxutil"
+	"github.com/hajimehoshi/ebiten"
+	"math"
 	"testing"
 )
 
+var img *ebiten.Image
+
+// Round to 3 decimals to ignore 10^-15 diffs
+func r(x float64) float64 {
+	return math.Round(x*1000) / 1000
+}
+
 func isDifferent(x, y, vx, vy float64, b *Ball) bool {
 
-	if x == b.Obj.X[0] && y == b.Obj.X[1] && vx == b.Obj.V[0] && vy == b.Obj.V[1] {
+	if x == r(b.Obj.X[0]) && y == r(b.Obj.X[1]) && vx == r(b.Obj.V[0]) && vy == r(b.Obj.V[1]) {
 		return false
 	}
 	return true
@@ -24,9 +33,11 @@ func isPresent(x, y, vx, vy, scale float64) bool {
 	return false
 }
 
-func TestList(t *testing.T) {
+func init() {
+	img, _ = gfxutil.LoadPngSlice(images.ImageStar)
+}
 
-	img, _ := gfxutil.LoadPngSlice(images.ImageStar)
+func TestList(t *testing.T) {
 
 	for _, c := range []struct {
 		x, y, vx, vy, scale float64
@@ -59,24 +70,32 @@ func TestList(t *testing.T) {
 
 func TestBoundary(t *testing.T) {
 
-	img, _ := gfxutil.LoadPngSlice(images.ImageStar)
-
 	for _, c := range []struct {
 		x0, y0, vx0, vy0 float64
 		x, y, vx, vy     float64
+		factor           float64
 	}{
-		// Not supposed to bounce
-		{1, 1, 1, 1, 1, 1, 1, 1},
-		{99, 99, -1, -1, 99, 99, -1, -1},
-		// Corner bounce
-		{1, 1, -1, -1, 1, 1, 1, 1},
-		{99, 99, 1, 1, 99, 99, -1, -1},
+		// In corners direction is inwards
+		{1, 1, 1, 1, 1, 1, 1, 1, 1},
+		{1, 99, 1, -1, 1, 99, 1, -1, 1},
+		{99, 1, -1, 1, 99, 1, -1, 1, 1},
+		{99, 99, -1, -1, 99, 99, -1, -1, 1},
+		// In corners and bounce
+		{1, 1, -1, -1, 1, 1, 1, 1, 1},
+		{1, 99, -1, 1, 1, 99, 1, -1, 1},
+		{99, 1, 1, -1, 99, 1, -1, 1, 1},
+		{99, 99, 1, 1, 99, 99, -1, -1, 1},
+		// Factor variations
+		{1, 1, 1, 1, 1, 1, 1, 1, 0.5},
+		{1, 1, 1, 1, 1, 1, 1, 1, 10},
+		{1, 1, -1, -1, 1, 1, 0.5, 0.5, 0.5},
+		{1, 1, -1, -1, 1, 1, 10, 10, 10},
 	} {
-		b, err := New(c.x0, c.y0, c.vx0, c.vy0, 1, img)
+		b, err := New(c.x0, c.y0, c.vx0, c.vy0, 1.0, img)
 		if err != nil {
 			t.Errorf("New(%f, %f, %f, %f, %f, %p) error %s", c.x0, c.y0, c.vx0, c.vy0, 1.0, img, err)
 		}
-		err = Boundary(b, 0.0, 100.0, 0.0, 100.0, 1.0)
+		err = Boundary(b, 0.0, 100.0, 0.0, 100.0, c.factor)
 		if err != nil {
 			t.Errorf("Boundary(%+v, 0, 1, 0, 1) error %s", b.Obj, err)
 		}
@@ -84,4 +103,30 @@ func TestBoundary(t *testing.T) {
 			t.Errorf("Ball with (%f, %f, %f, %f)) = %+v != (%f, %f, %f, %f)", c.x0, c.y0, c.vx0, c.vy0, b.Obj, c.x, c.y, c.vx, c.vy)
 		}
 	}
+}
+
+func TestCollide(t *testing.T) {
+
+	// Two balls within range
+	b1, _ := New(50.0, 50.0, 1.0, 1.0, 1.0, img)
+	b2, _ := New(51.0, 51.0, -1.0, -1.0, 1.0, img)
+	Collide(b1, b2)
+	if isDifferent(50.0, 50.0, -1.0, -1.0, b1) {
+		t.Errorf("Ball b1 is faulty %+v", b1.Obj)
+	}
+	if isDifferent(51.0, 51.0, 1.0, 1.0, b2) {
+		t.Errorf("Ball b2 is faulty %+v", b2.Obj)
+	}
+
+	// Two balls out of range
+	b3, _ := New(50.0, 50.0, 1.0, 1.0, 1.0, img)
+	b4, _ := New(55.0, 55.0, -1.0, -1.0, 1.0, img)
+	Collide(b3, b4)
+	if isDifferent(50.0, 50.0, 1.0, 1.0, b3) {
+		t.Errorf("Ball b3 is faulty %+v", b1.Obj)
+	}
+	if isDifferent(55.0, 55.0, -1.0, -1.0, b4) {
+		t.Errorf("Ball b4 is faulty %+v", b2.Obj)
+	}
+
 }
